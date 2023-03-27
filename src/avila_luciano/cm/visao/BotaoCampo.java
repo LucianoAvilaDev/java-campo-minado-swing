107710dab54b8084d160362dbd87e298653acbe1
LucianoAvilaDev <luciano.avila.dev@outlook.com>
2023-03-27 14:25:02 -0300

    minesweeper

--- src/avila_luciano/cm/visao/BotaoCampo.java
+++ src/avila_luciano/cm/visao/BotaoCampo.java
@@ -4,15 +4,16 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import avila_luciano.cm.modelo.Campo;
 import avila_luciano.cm.modelo.CampoEvento;
 import avila_luciano.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton implements CampoObservador{
-
-	private final Color BG_COLOR = new Color(184,184,184);
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener{
+	private final Color BG_PADRAO = new Color(184,184,184);
 	private final Color BG_MARCADO = new Color(8,179,247);
 	private final Color BG_EXPLODIR = new Color(189,66,68);
 	private final Color TEXTO_VERDE = new Color(0,100,0);
@@ -21,9 +22,10 @@ public class BotaoCampo extends JButton implements CampoObservador{
 	
 	public BotaoCampo(Campo campo) {
 		this.campo = campo;
-		setBackground(BG_COLOR);
+		setBackground(BG_PADRAO);
 		setBorder(BorderFactory.createBevelBorder(0));
-		
+		setOpaque(true);
+		addMouseListener(this);
 		campo.registrarObservador(this);
 	}
 
@@ -46,21 +48,79 @@ public class BotaoCampo extends JButton implements CampoObservador{
 	}
 
 	private void aplicarEstiloPadrao() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_PADRAO);
+		setText("");
 	}
 
 	private void aplicarEstiloExplodir() {
-		// TODO Auto-generated method stub
-		
+		setBackground(BG_EXPLODIR);
+		setForeground(Color.WHITE);
+		setText("X");
 	}
 
 	private void aplicarEstiloMarcar() {
+		setBackground(BG_MARCADO);
+		setForeground(Color.BLACK);
+		setText("M");
+	}
+
+	private void aplicarEstiloAbrir() {
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		if(campo.isMinado()) {
+			setBackground(BG_EXPLODIR);
+			return;
+		}
+		setBackground(BG_PADRAO);
+		
+		switch (campo.minasNaVizinhanca()) {
+		case 1:
+			setForeground(TEXTO_VERDE);
+			break;
+		case 2:
+			setForeground(Color.BLUE);
+			break;
+		case 3: 
+			setForeground(Color.YELLOW);
+			break;
+		case 4: 
+		case 5: 
+		case 6: 
+			setForeground(Color.RED);
+			break;
+		default:
+			setForeground(Color.PINK);
+		}
+		
+		String valor = !campo.vizinhancaSegura() ? campo.minasNaVizinhanca() + "" : "" ; 
+		setText(valor);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	private void aplicarEstiloAbrir() {
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
